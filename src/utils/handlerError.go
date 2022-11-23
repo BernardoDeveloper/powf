1848783826817ad21134ce546f6919307c749c19
BernardoDeveloper <bernardogualberto8@gmail.com>
2022-11-22 21:20:41 -0300

    feat: make see better errors

--- src/utils/handlerError.go
+++ src/utils/handlerError.go
@@ -2,11 +2,12 @@ package utils
 
 import (
 	"fmt"
+	"os"
 )
 
 func HttpErr(msg string, err error) {
 	if err != nil {
-		fmt.Println(msg)
+		fmt.Fprintf(os.Stdout, "\033[0;31m [ERROR]: { \033[0m %s \033[0;31m } \033[0m \n \033[0;31m [PANIC MSG]: \033[0m", msg)
 		panic(err)
 	}
 }
